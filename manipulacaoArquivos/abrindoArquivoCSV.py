2f63d59097808ab60ed3f81b81e09c63ea95a1b3
jvftw <jvmbworks@gmail.com>
2023-07-28 15:51:57 -0400

    manipulação de csv 1

--- manipulacaoArquivos/abrindoArquivoCSV.py
+++ manipulacaoArquivos/abrindoArquivoCSV.py
@@ -1,3 +1,5 @@
 arquivo = open('manipulacaoArquivos/dados.csv')
 dados = arquivo.read()
 arquivo.close()
+for registro in dados.splitlines():
+    print('Nome: {}, Idade: {}'.format(*registro.split(',')))
