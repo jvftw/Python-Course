b720296a95b6c2774e13668109f582fbf9ad3b1d
jvftw <jvmbworks@gmail.com>
2023-07-30 01:14:50 -0400

    comprehension 1 e 2

---
+++ comprehension/comprehension1.py
@@ -0,0 +1,9 @@
+dobros = [i * 2 for i in range (10)]
+#numeros de 0 até 9 serão serão multiplicados por 2
+print(dobros)
+
+#outra versão
+dobros = []
+for i in range(10):
+    dobros.append(i * 2)
+print(dobros)
